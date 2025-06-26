d6f4f7f4528495f487b51fc59140c5dfc65a4fbd
Jigar Karangiya <jigar.karangiya@brainvire.com>
2025-06-27 03:15:16 +0530

    Domain Link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,10 +20,12 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: `https://${organizationName}.github.io`,
+  // url: `https://${organizationName}.github.io`,
+  url: 'https://docs.jigarkarangiya.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: `/${projectName}/`,
+  // baseUrl: `/${projectName}/`,
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
