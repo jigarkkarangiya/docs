7539edb19f915ded256017281e2fdd3616208e9b
Jigar Karangiya <jigar.karangiya@brainvire.com>
2025-06-27 10:31:17 +0530

    docs: Change homepage action redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="docs/intro">
+            to="docs/category/magento-2-modules">
             Get Started
           </Link>
         </div>
